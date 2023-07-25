156aa8b493cd69ab9226ac1d4f5a93f30e8e4ead
postforty <75767073+postforty@users.noreply.github.com>
2023-07-25 23:05:40 +0900

    [ADD] controller

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
-	@GetMapping("/hello")
-	@ResponseBody
-	public String hello() {
-		return "Hello Spring Boot Board";
-	}
+public class MainController {
 
+	@GetMapping("/sbb")
+	@ResponseBody
+	public String index() {
+		System.out.println("index");
+		return "안녕하세요 sbb에 오신것을 환영합니다.";
+	}
 }
